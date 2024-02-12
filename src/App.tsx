bd6e9b2dfc7472d759d9b2c9d578ef3beacd7f1e
Snaflus <cani0006@stud.kea.dk>
2024-02-12 10:42:51 +0100

    created Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,28 @@
 import "./App.css";
+import { Grid, GridItem, Show } from "@chakra-ui/react";
+import NavBar from "./components/NavBar";
 
 function App() {
-  return <></>;
+  return (
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`,
+      }}
+    >
+      <GridItem bg="orange.300" gridArea={"nav"}>
+        <NavBar />
+      </GridItem>
+      <Show above="lg">
+        <GridItem bg="pink.300" gridArea={"aside"}>
+          Aside
+        </GridItem>
+      </Show>
+      <GridItem bg="green.300" gridArea={"main"}>
+        Main
+      </GridItem>
+    </Grid>
+  );
 }
 
 export default App;
